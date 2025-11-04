79f651044186283d3fbc2649351896a3443d99de
João Paulo <joaopaulodev01@gmail.com>
2025-11-04 20:08:04 -0300

    Remove unnecessary whitespace in script.js

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ function nextImage(){
     document.getElementById("radio"+count).checked = true;
 }
 
-const handlePhone = (event) => {
-  let input = event.target
-  input.value = phoneMask(input.value)
-}
-
-const phoneMask = (value) => {
-  if (!value) return ""
-  value = value.replace(/\D/g,'')
-  value = value.replace(/(\d{2})(\d)/,"($1) $2")
-  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
-  return value
-}
